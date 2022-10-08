9e1401cae3e4f3ba4d909c9b68f6a74cd5a40173
rattaponohz <62050147@go.buu.ac.th>
2022-10-08 17:31:54 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+    // const btnStart =document.querySelector(".btn-start")
+    // const imageLink = 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Tokyo_Sky_Tree_2012_%E2%85%A3.JPG';
+    // const downloadSize = 4605374;
+    // const time_start = new Date().getTime();; 
+    // const time_end = new Date().getTime();;
+    // const downloadSrc = new Image();
+    // const cacheImg = "?nn="+ time_start;
+
+    // const onClick = () =>{
+    // downloadSrc.src = imageLink + cacheImg;
+
+    // downloadSrc.onload = function(){
+    //     const timeDuration = time_end - time_start / 1000;
+    //     loadedBytes = downloadSize * 8,
+    //     totalSpeed = ((loadedBytes / timeDuration) /1024/1024).toFixed(2);
+    // }
+    //     console.log(downloadSrc);
+    //     document.querySelector('.content').innerHTML = totalSpeed + '<small>Mbps</small>'
+    // };
+
+    // btnStart.addEventListener("click",onClick)
